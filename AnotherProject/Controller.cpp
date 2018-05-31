68b2802d74c3d2e5c17142e39d03a7d132b20081
Yael_Amaya <yiap2610@gmail.com>
2018-05-31 14:46:53 -0600

    Fixed the vector to be an actual array

--- AnotherProject/Controller.cpp
+++ AnotherProject/Controller.cpp
@@ -15,22 +15,22 @@ Controller :: Controller()
 
 void Controller :: start()
 {
-    vector<int> counting(5);
+    int counting[5];
     for (int i = 1; i < 6; i++)
     {
         counting [i] = i;
         cout << "count is " << counting [i] << endl;
     }
-    
-    cout << "Calvin starts to pee" << endl;
-    vector<double> litersOfPiss(100.00);
-    for (double i = 1; i < 100.00; i += 0.01)
+
+    cout << "Random person starts saying 'Hi'" << endl;
+    double AmountOfHi[100];
+    for (int i = 1; i < 100; i += 1)
     {
-        litersOfPiss [i] = i;
-        cout << "Calvin has let out " << litersOfPiss [i] << " liters of piss" << endl;
+        AmountOfHi [i] = i * 1.156;
+        cout << "Random Person has said 'Hi' " << AmountOfHi [i] << " times" << endl;
     }
     
-    vector<string> amountOfStupid(3);
+    string amountOfStupid[3];
     amountOfStupid[1] = "Lvl_1";
     amountOfStupid[2] = "Lvl_2";
     amountOfStupid[3] = "Lvl_3";
